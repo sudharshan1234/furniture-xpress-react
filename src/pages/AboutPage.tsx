0c1c6569848a50b68e10125eec708a34cfe8fb68
Sudharshan Govindan <sudhuworks@gmail.com>
2023-08-07 14:00:34 +0530

    added about page

--- src/pages/AboutPage.tsx
+++ src/pages/AboutPage.tsx
@@ -15,10 +15,31 @@ const AboutPage = () => {
             <div className="w-24 h-1 bg-primary-500 ml-0 mr-auto"></div>
           </div>
           <p className="max-w-[45em] my-0 mx-auto mt-8 text-grey-500">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Et
-            possimus, repellat voluptatum libero quisquam laboriosam unde sequi
-            magnam ad facilis mollitia obcaecati. Impedit magni harum magnam
-            nostrum hic quam eligendi.
+            Welcome to FurnitureXpress, your one-stop destination for premium
+            furniture solutions that perfectly blend style, comfort, and
+            functionality. Our mission is to revolutionize the furniture
+            shopping experience and make it enjoyable, seamless, and secure for
+            every customer.
+            <br></br>
+            <br></br>
+            At FurnitureXpress, we curate an extensive collection of handpicked
+            furniture pieces designed to elevate your home or office
+            environment. From elegant accent chairs to durable wooden desks, our
+            carefully selected range caters to diverse tastes and interior
+            design preferences. Embracing innovation, we offer a seamless login,
+            logout, and signup functionality to ensure a personalized and secure
+            shopping journey for each user.
+            <br></br>
+            <br></br>
+            We take pride in our dedication to customer satisfaction, offering
+            top-notch customer service and incorporating Stripe payment for
+            smooth and secure transactions. Our platform goes beyond just
+            showcasing furniture; it inspires creativity and transformation,
+            making it easier than ever to turn your living spaces into
+            personalized havens of style and comfort. Discover the art of
+            furniture shopping redefined at FurnitureXpress, and embark on a
+            delightful journey of furnishing your spaces with elegance and
+            grace.
           </p>
         </article>
       </section>
